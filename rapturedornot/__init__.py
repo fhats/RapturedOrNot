1558f5de05635534ff317911a64c5d69c781d0da
Stephen Johnson <srj15@case.edu>
2011-05-21 02:48:35 -0700

    Screw you caching

--- rapturedornot/__init__.py
+++ rapturedornot/__init__.py
@@ -1,7 +1,13 @@
+import time
+
 from flask import Flask
 import settings
 
 app = Flask('rapturedornot')
 app.config.from_object('rapturedornot.settings')
+
+@app.context_processor
+def inject_stupid():
+    return dict(stupidtime=str(time.time()))
 
 import views
